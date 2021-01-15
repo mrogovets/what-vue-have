7d42f4b306d0c31ac87596a448e0c6174e52de93
mrogovets <34492317+mrogovets@users.noreply.github.com>
2021-01-15 21:00:34 +0200

    01:52:03

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 Vue.createApp({
   data: () => ({
+    myHtml: "<h1>Vue 3 App</h1>",
     title: "I'm Groot",
+    person: {
+      firstName: "Maksym",
+      lastName: "Rohovets",
+      age: 41,
+    },
+    items: [1, 2, 3, 4, 5, 6],
   }),
 }).mount("#app");
