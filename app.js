058204ce78f77cda9b3df12630573873fde120e4
mrogovets <34492317+mrogovets@users.noreply.github.com>
2021-02-02 23:13:06 +0200

    end of 1-st part

--- app.js
+++ app.js
@@ -7,6 +7,24 @@ Vue.createApp({
       lastName: "Rohovets",
       age: 41,
     },
-    items: [1, 2, 3, 4, 5, 6],
+    items: [1, 2],
   }),
+  methods: {
+    addItem(event) {
+      this.items.unshift(this.$refs.myInput.value);
+      this.$refs.myInput.value = "";
+      console.log(event.key);
+    },
+    remove(i) {
+      this.items.splice(i, 1);
+    },
+    log(item) {
+      console.log(item);
+    },
+  },
+  computed: {
+    evenItems() {
+      return this.items.filter((i) => i % 2 === 0);
+    },
+  },
 }).mount("#app");
